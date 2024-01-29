9ced1f8353a376cecf3ee3df15e94769bed099f3
unknown <igor.mokrushin.159@mail.ru>
2024-01-29 21:15:55 +0500

    getters and setters in entity avatar have been removed

--- src/main/java/ru/hogwarts/school/model/Avatar.java
+++ src/main/java/ru/hogwarts/school/model/Avatar.java
@@ -1,11 +1,13 @@
 package ru.hogwarts.school.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.util.Arrays;
 import java.util.Objects;
 
 @Entity
+@Data
 @Table(name = "avatar")
 public class Avatar {
     @Id
@@ -16,78 +18,4 @@ public class Avatar {
     private String mediaType;
     private byte[] data;
 
-
-    public Avatar() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFilePath() {
-        return filePath;
-    }
-
-    public void setFilePath(String filePath) {
-        this.filePath = filePath;
-    }
-
-    public Long getFileSize() {
-        return fileSize;
-    }
-
-    public void setFileSize(Long fileSize) {
-        this.fileSize = fileSize;
-    }
-
-    public String getMediaType() {
-        return mediaType;
-    }
-
-    public void setMediaType(String mediaType) {
-        this.mediaType = mediaType;
-    }
-
-    public byte[] getData() {
-        return data;
-    }
-
-    public void setData(byte[] data) {
-        this.data = data;
-    }
-
-
-    public void setFileSize(long fileSize) {
-        this.fileSize = fileSize;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Avatar avatar = (Avatar) o;
-        return fileSize == avatar.fileSize && Objects.equals(id, avatar.id) && Objects.equals(filePath, avatar.filePath) && Objects.equals(mediaType, avatar.mediaType) && Arrays.equals(data, avatar.data);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = Objects.hash(id, filePath, fileSize, mediaType);
-        result = 31 * result + Arrays.hashCode(data);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "Avatar{" +
-                "id=" + id +
-                ", filePath='" + filePath + '\'' +
-                ", fileSize=" + fileSize +
-                ", mediaType='" + mediaType + '\'' +
-                ", data=" + Arrays.toString(data) +
-                '}';
-    }
 }
