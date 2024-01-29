6c55f8df44abed4c234e34b52f04283cdd1291b7
unknown <igor.mokrushin.159@mail.ru>
2024-01-29 21:12:54 +0500

    gegetters and setters in entity student have been removed

--- src/main/java/ru/hogwarts/school/model/Student.java
+++ src/main/java/ru/hogwarts/school/model/Student.java
@@ -37,67 +37,7 @@ public class Student {
     public Student() {
     }
 
-//    public House getHouse() {
-//        return house;
-//    }
-//
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public int getAge() {
-//        return age;
-//    }
-//
-//    public void setAge(int age) {
-//        this.age = age;
-//    }
-//
-//    public void setHouse(House house) {
-//        this.house = house;
-//    }
-//
-//
-//    public void setAvatar(Avatar avatar) {
-//        this.avatar = avatar;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "Student{" +
-//                "id=" + id +
-//                ", name='" + name + '\'' +
-//                ", age=" + age +
-//                ", house=" + house +
-//                ", avatar=" + avatar +
-//                '}';
-//    }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        Student student = (Student) o;
-//        return age == student.age && Objects.equals(id, student.id) && Objects.equals(name, student.name) && Objects.equals(house, student.house) && Objects.equals(avatar, student.avatar);
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(id, name, age, house, avatar);
-//    }
+
 }
 
 
